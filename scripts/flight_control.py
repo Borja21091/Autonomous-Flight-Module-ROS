Projection calculation now correct

scripts/flight_control.py
@@ -277,33 +277,27 @@ class pathPlanner():
             # Project Drone 3D (base_link) position to 2D
             rG = pos - pos_1
             r_drone = np.matmul(np.transpose(rot),rG)
-            projPos3D = self.projectVec2Plane(r_drone,self.n)
+            rProj = self.projectVec2Plane(r_drone,self.n)
             pos_1 = np.copy(pos)
             
             # Project velocities 2D --> 3D local
-            if self.firstTime:
-                pos0 = projPos3D # pos
-                self.proj.projVelFirstTime(pos0)
-                self.firstTime = False
-
-            else:
-                # Linear Velocity
-                velParam = np.empty([3,1])
-                velParam = self.proj.projVel(projPos3D)
-                self.vel_output[0] = self.distance_control()
-                self.vel_output[1] = velParam[1,0]
-                self.vel_output[2] = velParam[2,0]
-                self.vel_output = self.drone.v_max*self.vel_output / np.linalg.norm(self.vel_output)
-                # Angular Velocity
-                self.w_z = self.rotation_control()
-                self.w_z = min(max(-self.drone.w_max, self.w_z),self.drone.w_max)
-                rospy.loginfo('Rotation target: %s', self.w_z)
-                if self.proj.t_i > self.proj.t_end:
-                    self.vel_output[0] = 0.0
-                    self.vel_output[1] = 0.0
-                    self.vel_output[2] = 0.0
-                    self.w_z = 0.0
-                    rospy.loginfo("Stopping Motion")
+            # Linear Velocity
+            velParam = np.empty([3,1])
+            velParam = self.proj.projVel(rProj)
+            self.vel_output[0] = self.distance_control()
+            self.vel_output[1] = velParam[1,0]
+            self.vel_output[2] = velParam[2,0]
+            self.vel_output = self.drone.v_max*self.vel_output / np.linalg.norm(self.vel_output)
+            # Angular Velocity
+            self.w_z = self.rotation_control()
+            self.w_z = min(max(-self.drone.w_max, self.w_z),self.drone.w_max)
+            rospy.loginfo('Rotation target: %s', self.w_z)
+            if self.proj.t_i > self.proj.t_end:
+                self.vel_output[0] = 0.0
+                self.vel_output[1] = 0.0
+                self.vel_output[2] = 0.0
+                self.w_z = 0.0
+                rospy.loginfo("Stopping Motion")
                     
             # Merge local velocities to Twist ROS msg
             self.merge2Twist(self.drone.vel,self.vel_output,self.w_z)

scripts/project_trajectory.py
@@ -34,6 +34,8 @@ class project_trajectory(object):
         self.Cv = Cv
         # Integral of error for controller
         self.sErrorProj = np.zeros([2,1])
+        # Planar position
+        self.xProj = np.zeros([2,1])
         
         # Publisher
         self.drone2D_pub = rospy.Publisher('drone2D', PointStamped, queue_size = 10)
@@ -78,10 +80,10 @@ class project_trajectory(object):
         alpha = 0.3
 
         # Project global drone prosition and subtract reference point (origin of parametric trajectory)
-        xProj = np.matmul(self.projection_matrix,pos) - self.x_proj_ref
+        self.xProj += np.matmul(self.projection_matrix,pos)
         
         # Publish 2D projected position
-        self.publish_dronePos_2D(xProj)
+        self.publish_dronePos_2D(self.xProj)
 
         # Loop to find closest point in trajectory to real projected position
         xTraj = np.empty([2,1])
@@ -96,8 +98,8 @@ class project_trajectory(object):
             # Get derivative for given t
             dxTraj = self.traj.dpos(self.t)
             # Distance between trajectory and real projected drone position
-            errorProj[0] = xTraj[0] - xProj[0]
-            errorProj[1] = xTraj[1] - xProj[1]
+            errorProj[0] = xTraj[0] - self.xProj[0]
+            errorProj[1] = xTraj[1] - self.xProj[1]
             # Derivative of the distance at t
             DC = np.matmul(np.transpose(errorProj),dxTraj)
             # Update trajectory parameter t according to gradient
@@ -115,7 +117,7 @@ class project_trajectory(object):
             
         # Update parameter t global
         self.t_i = self.t
-        self.projection = xProj
+        self.projection = self.xProj
         self.trajectory = xTraj
 
         # Compute planar velocity